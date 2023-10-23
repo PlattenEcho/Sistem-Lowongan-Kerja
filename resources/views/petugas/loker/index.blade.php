d2519c3ec746ab55342e5f2b5b0fb83d498a8abe
PlattenEcho <akunsekolahdika@gmail.com>
2023-10-23 08:35:17 +0700

    add navigasi tombol tambah loker

--- resources/views/petugas/loker/index.blade.php
+++ resources/views/petugas/loker/index.blade.php
@@ -4,7 +4,7 @@
     <h1
         class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
         Daftar Lowongan Kerja</h1>
-    <button type="button"
+    <button href="{{ route('tambah-loker') }}" type="button"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+
         Tambah Loker</button>
 

6b72ede608a431a5a1a1e74157bb61e0e62dd2f7
PinKevin <emeriokevin2320@gmail.com>
2023-10-23 10:48:47 +0700

    Ganti button ke a untuk tambah loker di index (tampilan error)

--- resources/views/petugas/loker/index.blade.php
+++ resources/views/petugas/loker/index.blade.php
@@ -4,12 +4,12 @@
     <h1
         class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
         Daftar Lowongan Kerja</h1>
-    <button href="{{ route('tambah-loker') }}" type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">+
-        Tambah Loker</button>
+    <a href="{{ route('tambah-loker') }}"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        + Tambah Loker
+    </a>
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-
 
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">

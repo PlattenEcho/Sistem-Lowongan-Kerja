94cae674aa5e995cdc858b5a6424b21e362361cc
PinKevin <emeriokevin2320@gmail.com>
2023-10-24 16:00:59 +0700

    Fix detail loker (masih error tahap 2)

--- resources/views/petugas/loker/detail.blade.php
+++ resources/views/petugas/loker/detail.blade.php
@@ -4,7 +4,7 @@
     <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Detail Lowongan Kerja</h2>
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-            <form action="" method="GET">
+            <form action="" method="">
                 @csrf
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">

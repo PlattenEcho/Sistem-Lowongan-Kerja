ad7bc1074c33e782cbcab7566d4a9c9e526ec765
Dzahwan <mochammaddzahwan@students.undip.ac.id>
2023-10-24 23:33:26 +0700

    Create and Update Loker (-routes update)

--- resources/views/petugas/loker/index.blade.php
+++ resources/views/petugas/loker/index.blade.php
@@ -53,7 +53,8 @@
                             {{ $loker->perusahaan->nama }}
                         </td>
                         <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="{{ route('edit-loker', ['id' => $loker->id]) }}"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                         </td>
                     </tr>
                 @endforeach

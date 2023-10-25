bf4a1ee2eb61e8d47ffe874339dee5d6a5ed29d9
PinKevin <emeriokevin2320@gmail.com>
2023-10-26 00:29:44 +0700

    Perbaikan tampilan create

--- resources/views/petugas/loker/create.blade.php
+++ resources/views/petugas/loker/create.blade.php
@@ -1,18 +1,34 @@
 @extends('templates.dashboard-petugas')
 
 @section('dashboard-content')
-    <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Tambah Lowongan Kerja</h2>
-    <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+    <h1
+        class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
+        Tambah Lowongan Kerja
+    </h1>
+    <a href="{{ route('daftar-loker') }}"
+        class="text-white bg-gray-600 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm inline-block px-3 py-2.5 mx-auto mb-3 dark:bg-white-600 dark:hover-bg-white-700 focus:outline-none dark:focus:ring-white-800 w-full"
+        style="max-width: 110px;">
+        <div class="flex flex-row items-center justify-center">
+            <svg class="w-4 h-3 text-white-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" />
+            </svg>
+            <span class="ml-0.5 ">Kembali</span>
+        </div>
+    </a>
+
+    <section class="bg-white dark:bg-gray-900 relative mt-auto mx-auto overflow-x-auto sm:rounded-lg">
+        <div class="py-4 px-auto mx-px max-w-full lg:py-auto">
             <form action="{{ route('simpan-loker') }}" method="POST">
                 @csrf
-                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div class="sm:col-span-2">
                         <label for="nama"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
                         <input type="text" name="nama" id="nama"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="Desainer" min="17">
+                            placeholder="Contoh : Desainer">
                     </div>
                     <div class="sm:col-span-2">
                         <label for="tipe"
@@ -32,46 +48,57 @@
                             Minimal</label>
                         <input type="number" name="usia_min" id="usia_min"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="17" min="17" max="70">
+                            placeholder="Contoh : 17" min="17" max="70">
                     </div>
                     <div class="w-full">
                         <label for="usia_max" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usia
                             Maksimal</label>
                         <input type="number" name="usia_max" id="usia_max"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="70" min="17" max="70">
+                            placeholder="Contoh : 70" min="17" max="70">
                     </div>
                     <div class="w-full">
                         <label for="gaji_min" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gaji
                             Minimal</label>
                         <input type="number" name="gaji_min" id="gaji_min"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="1000000">
+                            placeholder="Contoh : 1000000">
                     </div>
                     <div class="w-full">
                         <label for="gaji_max" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gaji
                             Maksimal</label>
                         <input type="number" name="gaji_max" id="gaji_max"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="30000000">
+                            placeholder=" Contoh : 30000000">
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="nama_cp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Perusahaan
+                        </label>
+                        <input type="text" name="nama_cp" id="nama_cp"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Contoh : HYBE">
                     </div>
                     <div class="sm:col-span-2">
                         <label for="nama_cp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
                             Narahubung</label>
                         <input type="text" name="nama_cp" id="nama_cp"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Contoh : Kang Haerin">
                     </div>
                     <div class="sm:col-span-2">
                         <label for="email_cp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email
                             Narahubung</label>
                         <input type="email" name="email_cp" id="email_cp"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Contoh : newjeans@gmail.com">
                     </div>
                     <div class="sm:col-span-2">
                         <label for="no_telp_cp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor
                             Telepon Narahubung</label>
                         <input type="text" name="no_telp_cp" id="no_telp_cp"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Contoh : 0812XXXXXXXX">
                     </div>
                     <div class="w-full">
                         <label for="tgl_aktif" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Batas
@@ -101,10 +128,6 @@
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 mr-2 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     + Tambah Loker
                 </button>
-                <a href="{{ route('daftar-loker') }}"
-                    class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 mr-2 mb-3 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                    Kembali
-                </a>
             </form>
         </div>
     </section>

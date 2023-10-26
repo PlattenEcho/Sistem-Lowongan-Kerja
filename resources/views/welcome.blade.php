a3d88e8c2f9a17bc634028fe00e663cad5a9005a
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-10-26 17:53:27 +0700

    add: styling pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,40 +1,26 @@
 @extends('templates.main')
 
 @section('body')
-    <section class="bg-white dark:bg-gray-900">
-        <div class="jumbotron-content-div">
-            <h1 class="jumbotron-h1">We invest in the world’s potential</h1>
-            <p class="jumbotron-p">Here at Flowbite we focus on markets where technology, innovation, and capital can
-                unlock long-term value and drive economic growth.</p>
-            <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="/login"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Login dulu gih
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
-                <a href="#"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Learn more
-                </a>
-            </div>
-            @auth
-                <br>
-                <a href="/dashboard"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Ke Dashboard {{ auth()->user()->name }}
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
-            @else
-                <h1>Login dulu rek</h1>
-            @endauth
+<section class="bg-white dark:bg-gray-900">
+    <div class="jumbotron-content-div">
+        <h1 class="jumbotron-h1">SISCAKER</h1>
+        <p class="jumbotron-p">Topik 4: Sistem Seleksi Lowongan Kerja (nomor 6 sampai 10)</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="/login" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Login Page
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+            </a>
         </div>
-    </section>
+        @auth
+        <br>
+        <a href="/dashboard" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Logged as {{ auth()->user()->name}}
+        </a>
+        @else
+        <br>
+        <h1>PBP Mini Project</h1>
+        @endauth
+    </div>
+</section>
 @endsection

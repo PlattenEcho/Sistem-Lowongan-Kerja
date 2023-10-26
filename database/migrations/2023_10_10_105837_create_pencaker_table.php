dab17f03874b4c24d8e628772dc1d1504a0e65bc
PlattenEcho <akunsekolahdika@gmail.com>
2023-10-26 22:23:50 +0700

    add show foto and foto ktp di page administrasi

--- database/migrations/2023_10_10_105837_create_pencaker_table.php
+++ database/migrations/2023_10_10_105837_create_pencaker_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -24,6 +23,8 @@ return new class extends Migration
             $table->string('alamat');
             $table->string('kota');
             $table->string('no_telp');
+            $table->string('foto_profil')->nullable();
+            $table->string('foto_ktp')->nullable();
         });
     }
 

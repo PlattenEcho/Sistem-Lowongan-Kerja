fb82cdf23919e87959d4880a08e775aa690bf5fa
PinKevin <emeriokevin2320@gmail.com>
2023-10-28 02:57:34 +0700

    Buat logika pilih pencaker yang akan diseleksi wawancara

--- app/Http/Controllers/ApplyLokerController.php
+++ app/Http/Controllers/ApplyLokerController.php
@@ -11,21 +11,33 @@ class ApplyLokerController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() {
-        $applyLokers = ApplyLoker::whereNotIn('id', function($query) {
-                $query->select('id_apply')->from('tahapan_apply');
-            })
-            ->whereHas('loker', function($query) {
+    public function index()
+    {
+        $applyLokersForAdmin = ApplyLoker::whereNotIn('id', function ($query) {
+            $query->select('id_apply')->from('tahapan_apply');
+        })
+            ->whereHas('loker', function ($query) {
                 $query->where('status', 'Sedang Seleksi');
             })
             ->get();
-            
-        return view('petugas.apply-loker.index', compact('applyLokers'));
-    }
-    
-    
 
-    
+        $applyLokersForInterview = ApplyLoker::whereIn('id', function ($query) {
+            $query->select('id_apply')
+                ->from('tahapan_apply')
+                ->where('id_tahapan', 1)
+                ->where('nilai', 1);
+        })
+            ->whereHas('loker', function ($query) {
+                $query->where('status', 'Sedang Seleksi');
+            })
+            ->get();
+
+        return view('petugas.apply-loker.index', compact('applyLokersForAdmin', 'applyLokersForInterview'));
+    }
+
+
+
+
     /**
      * Show the form for creating a new resource.
      */
--- resources/views/petugas/apply-loker/index.blade.php
+++ resources/views/petugas/apply-loker/index.blade.php
@@ -1,32 +1,45 @@
 @extends('templates.dashboard-petugas')
 
 @section('dashboard-content')
-<div class="container">
-    <h1 class="jumbotron-h1">
-    Daftar Apply Loker Belum Seleksi</h1>
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="head-table">
-            <tr>
-                <th scope="col" class="px-6 py-3">No.</th>
-                <th scope="col" class="px-6 py-3">Nama Pekerjaan</th>
-                <th scope="col" class="px-6 py-3">Nama Pencari Kerja</th>
-                <th scope="col" class="px-6 py-3">Tanggal Apply</th>
-                <th scope="col" class="px-6 py-3">Seleksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($applyLokers as $applyLoker)
-                <tr class="row-table">
-                    <td class="px-6 py-4">{{ $loop->index + 1 }}</td>
-                    <td class="px-6 py-4">{{ $applyLoker->loker->nama }}</td>
-                    <td class="px-6 py-4">{{ $applyLoker->pencaker->nama }}</td>
-                    <td class="px-6 py-4">{{ $applyLoker->tgl_apply }}</td>
-                    <td class="px-6 py-4" >
-                            <a href="{{ route('apply-loker.show', ['id' => $applyLoker->id]) }}" class="blue-hyperlink">Administrasi</a>
-                    </td>
+    <div class="container">
+        <h1 class="jumbotron-h1">
+            Daftar Apply Loker Belum Seleksi</h1>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="head-table">
+                <tr>
+                    <th scope="col" class="px-6 py-3">No.</th>
+                    <th scope="col" class="px-6 py-3">Nama Pekerjaan</th>
+                    <th scope="col" class="px-6 py-3">Nama Pencari Kerja</th>
+                    <th scope="col" class="px-6 py-3">Tanggal Apply</th>
+                    <th scope="col" class="px-6 py-3">Menunggu Seleksi</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($applyLokersForAdmin as $applyLoker)
+                    <tr class="row-table">
+                        <td class="px-6 py-4">{{ $loop->index + 1 }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->loker->nama }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->pencaker->nama }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->tgl_apply }}</td>
+                        <td class="px-6 py-4">
+                            <a href="{{ route('apply-loker.show', ['id' => $applyLoker->id]) }}"
+                                class="blue-hyperlink">Administrasi</a>
+                        </td>
+                    </tr>
+                @endforeach
+                @foreach ($applyLokersForInterview as $applyLoker)
+                    <tr class="row-table">
+                        <td class="px-6 py-4">{{ $loop->index + 1 }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->loker->nama }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->pencaker->nama }}</td>
+                        <td class="px-6 py-4">{{ $applyLoker->tgl_apply }}</td>
+                        <td class="px-6 py-4">
+                            <a href="{{ route('apply-loker.show', ['id' => $applyLoker->id]) }}"
+                                class="blue-hyperlink">Wawancara</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection

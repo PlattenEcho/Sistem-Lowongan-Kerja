2780f694d878cd61db31e4ff3365e62b9c36d72c
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-10-28 16:09:48 +0700

    fix inactive nav styling

--- resources/views/templates/dashboard-petugas.blade.php
+++ resources/views/templates/dashboard-petugas.blade.php
@@ -75,28 +75,28 @@
             $applyLokerActive = request()->is('apply-loker*');
             @endphp
             <li>
-                <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-white rounded-lg {{ $dashboardActive ? 'bg-blue-500' : 'bg-gray-300' }} group">
-                    <svg class="w-5 h-5 {{ $dashboardActive ? 'text-white' : 'text-gray-500 dark:text-gray-400' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-white rounded-lg {{ $dashboardActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                         <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                         <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                     </svg>
-                    <span class="ml-3">Dashboard</span>
+                    <span class="ml-3 {{ $dashboardActive ? '' : 'text-black' }}">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('daftar-loker') }}" class="flex items-center p-2 text-white rounded-lg {{ $daftarLokerActive ? 'bg-blue-500' : 'bg-gray-300' }} group">
-                    <svg class="w-5 h-5 {{ $daftarLokerActive ? 'text-white' : 'text-gray-500 dark:text-gray-400' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                <a href="{{ route('daftar-loker') }}" class="flex items-center p-2 text-white rounded-lg {{ $daftarLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                    <svg class="w-5 h-5" aria-hidden="{{ $dashboardActive ? '' : 'text-black' }}" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                         <path d="M16 1h-3.278A1.992 1.992 0 0 0-1 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v2H7V2ZM5 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm8 4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Zm0-4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Z" />
                     </svg>
-                    <span class="ml-3">Daftar Lowongan</span>
+                    <span class="ml-3 {{ $daftarLokerActive ? '' : 'text-black' }}">Daftar Lowongan</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('apply-loker.index') }}" class="flex items-center p-2 text-white rounded-lg {{ $applyLokerActive ? 'bg-blue-500' : 'bg-gray-300' }} group">
-                    <svg class="w-5 h-5 {{ $applyLokerActive ? 'text-white' : 'text-gray-500 dark:text-gray-400' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <a href="{{ route('apply-loker.index') }}" class="flex items-center p-2 text-white rounded-lg {{ $applyLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                     </svg>
-                    <span class="ml-3">Administrasi</span>
+                    <span class="ml-3 {{ $applyLokerActive ? '' : 'text-black' }}">Administrasi</span>
                 </a>
             </li>
         </ul>

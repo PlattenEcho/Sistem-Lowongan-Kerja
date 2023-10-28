610099888b796a1d681ab56f7c32d71223c7ca56
PinKevin <emeriokevin2320@gmail.com>
2023-10-28 23:15:22 +0700

    Fix sidebar

--- resources/views/templates/dashboard-petugas.blade.php
+++ resources/views/templates/dashboard-petugas.blade.php
@@ -1,147 +1,121 @@
 @extends('templates.main')
 
 @section('body')
-<nav class="fixed top-0 z-50 w-full bg-gradient-to-r from-red-500 to-indigo-600 text-white py-1">
-    <div class="px-3 py-3 lg:px-5 lg:pl-3">
-        <div class="flex items-center justify-between">
-            <div class="flex items-center justify-start">
-                <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                    <span class="sr-only">Open sidebar</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                        </path>
-                    </svg>
-                </button>
-                <a href="{{ route('dashboard') }}" class="flex ml-2 md:mr-24">
-
-                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">SISCAKER</span>
-                </a>
-            </div>
-            <div class="flex items-center">
-                <div class="flex items-center ml-3">
-                    <div>
-                        <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
-                        </button>
-                    </div>
-                    <div class="dropdown-user-container" id="dropdown-user">
-                        <div class="px-4 py-3" role="none">
-                            <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                {{ auth()->user()->name }}
-                            </p>
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                {{ auth()->user()->email }}
-                            </p>
+    <nav class="fixed top-0 z-50 w-full bg-gradient-to-r from-red-500 to-indigo-600 text-white py-1">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start">
+                    <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
+                        type="button"
+                        class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <span class="sr-only">Open sidebar</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd" fill-rule="evenodd"
+                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                            </path>
+                        </svg>
+                    </button>
+                    <a href="{{ route('dashboard') }}" class="flex ml-2 md:mr-24">
+                        <span
+                            class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">SISCAKER</span>
+                    </a>
+                </div>
+                <div class="flex items-center">
+                    <div class="flex items-center ml-3">
+                        <div>
+                            <button type="button"
+                                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="w-8 h-8 rounded-full"
+                                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                            </button>
                         </div>
-                        <ul class="py-1" role="none">
-                            <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
-                            </li>
-                            {{-- <li>
-                                    <a href="#"
+                        <div class="dropdown-user-container" id="dropdown-user">
+                            <div class="px-4 py-3" role="none">
+                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    {{ auth()->user()->name }}
+                                </p>
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    {{ auth()->user()->email }}
+                                </p>
+                            </div>
+                            <ul class="py-1" role="none">
+                                <li>
+                                    <a href="{{ route('dashboard') }}"
                                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Settings</a>
+                                        role="menuitem">Dashboard</a>
                                 </li>
                                 <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Earnings</a>
-                                </li> --}}
-                            <li>
-                                {{-- <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Sign out</a> --}}
-                                <form action="/logout" method="post" class="block h-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300
-                                    dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
-                                    @csrf
-                                    <button type="submit" class="h-full w-full text-left">Log out</button>
-                                </form>
-                            </li>
-                        </ul>
+                                    <form action="/logout" method="post"
+                                        class="block h-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300
+                                    dark:hover:bg-gray-600 dark:hover:text-white"
+                                        role="menuitem">
+                                        @csrf
+                                        <button type="submit" class="h-full w-full text-left">Log out</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
-            @php
-            $dashboardActive = request()->is('dashboard*');
-            $daftarLokerActive = request()->is('daftar-loker*');
-            $applyLokerActive = request()->is('apply-loker*');
-            @endphp
-            <li>
-                <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-white rounded-lg {{ $dashboardActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                    </svg>
-                    <span class="ml-3 {{ $dashboardActive ? '' : 'text-black' }}">Dashboard</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('daftar-loker') }}" class="flex items-center p-2 text-white rounded-lg {{ $daftarLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="{{ $dashboardActive ? '' : 'text-black' }}" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                        <path d="M16 1h-3.278A1.992 1.992 0 0 0-1 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v2H7V2ZM5 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm8 4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Zm0-4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Z" />
-                    </svg>
-                    <span class="ml-3 {{ $daftarLokerActive ? '' : 'text-black' }}">Daftar Lowongan</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('apply-loker.index') }}" class="flex items-center p-2 text-white rounded-lg {{ $applyLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                    </svg>
-                    <span class="ml-3 {{ $applyLokerActive ? '' : 'text-black' }}">Administrasi</span>
-                </a>
-            </li>
-        </ul>
-    </div>
-</aside>
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
-            @php
-            $dashboardActive = request()->is('dashboard*');
-            $daftarLokerActive = request()->is('daftar-loker*');
-            $applyLokerActive = request()->is('apply-loker*');
-            @endphp
-            <li>
-                <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-white rounded-lg {{ $dashboardActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                    </svg>
-                    <span class="ml-3 {{ $dashboardActive ? '' : 'text-black' }}">Dashboard</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('daftar-loker') }}" class="flex items-center p-2 text-white rounded-lg {{ $daftarLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="{{ $dashboardActive ? '' : 'text-black' }}" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                        <path d="M16 1h-3.278A1.992 1.992 0 0 0-1 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v2H7V2ZM5 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm8 4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Zm0-4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Z" />
-                    </svg>
-                    <span class="ml-3 {{ $daftarLokerActive ? '' : 'text-black' }}">Daftar Lowongan</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('apply-loker.index') }}" class="flex items-center p-2 text-white rounded-lg {{ $applyLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                    </svg>
-                    <span class="ml-3 {{ $applyLokerActive ? '' : 'text-black' }}">Administrasi</span>
-                </a>
-            </li>
-        </ul>
-    </div>
-</aside>
+    @php
+        $dashboardActive = request()->is('dashboard*');
+        $daftarLokerActive = request()->is('daftar-loker*');
+        $applyLokerActive = request()->is('apply-loker*');
+    @endphp
+    <aside id="logo-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+            <ul class="space-y-2 font-medium">
+                <li>
+                    <a href="{{ route('dashboard') }}"
+                        class="flex items-center p-2 text-white rounded-lg {{ $dashboardActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 22 21">
+                            <path
+                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                            <path
+                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                        </svg>
+                        <span class="ml-3 {{ $dashboardActive ? '' : 'text-black' }}">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('daftar-loker') }}"
+                        class="flex items-center p-2 text-white rounded-lg {{ $daftarLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                        <svg class="w-5 h-5" aria-hidden="{{ $dashboardActive ? '' : 'text-black' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                            <path
+                                d="M16 1h-3.278A1.992 1.992 0 0 0-1 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v2H7V2ZM5 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm8 4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Zm0-4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Z" />
+                        </svg>
+                        <span class="ml-3 {{ $daftarLokerActive ? '' : 'text-black' }}">Daftar Lowongan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('apply-loker.index') }}"
+                        class="flex items-center p-2 text-white rounded-lg {{ $applyLokerActive ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} group">
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                        </svg>
+                        <span class="ml-3 {{ $applyLokerActive ? '' : 'text-black' }}">Seleksi</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </aside>
 
-<div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-        @yield('dashboard-content')
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+            @yield('dashboard-content')
+        </div>
     </div>
-</div>
 @endsection
